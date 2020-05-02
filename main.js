69fb98a9494baa84ce60342fdcb3036bc93e4062
Brian <brian.trundy@gmail.com>
2020-05-02 13:59:07 -0400

    Add google front page project files

---
+++ main.js
@@ -0,0 +1,4 @@
+const myHeading = document.querySelector('h1');
+const myHeading2 = document.querySelector('h1');
+myHeading.textContent = 'Replaced by JS!';
+myHeading2.textContent = 'Replaced by JS2!';
